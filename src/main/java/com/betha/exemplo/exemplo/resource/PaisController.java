040558913724c20328c4b3b5a2090a024c7fd0c5
Willian Dagostin Bittencourt <williandb@gmail.com>
2021-09-04 11:49:36 -0300

    teste

--- src/main/java/com/betha/exemplo/exemplo/resource/PaisController.java
+++ src/main/java/com/betha/exemplo/exemplo/resource/PaisController.java
@@ -3,10 +3,9 @@ package com.betha.exemplo.exemplo.resource;
 import com.betha.exemplo.exemplo.model.Pais;
 import com.betha.exemplo.exemplo.repository.PaisRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 @RestController
@@ -18,5 +17,27 @@ public class PaisController {
     @GetMapping
     public List<Pais> getPaises(){
         return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Pais getPaisesId(@PathVariable(value="id") Long paisId) throws EntityNotFoundException{
+        Pais paisFind = repository.findById(paisId).orElseThrow(() -> new EntityNotFoundException("País não encontrado com ID: "+ paisId));
+        return paisFind;
+    }
+
+    @PostMapping
+    public Pais create(@RequestBody Pais pais){
+        return repository.save(pais);
+    }
+
+    @PutMapping("/{id}")
+    public Pais update(@PathVariable(value = "id") Long paisId,
+                       @RequestBody Pais pais) throws EntityNotFoundException {
+        Pais paisFind = repository.findById(paisId).orElseThrow(() -> new EntityNotFoundException("País não encontrado com ID: " + paisId));
+        paisFind.setId(pais.getId());
+        paisFind.setNome(pais.getNome());
+        paisFind.setPopulacao(pais.getPopulacao());
+
+        return repository.save(paisFind);
     }
 }
